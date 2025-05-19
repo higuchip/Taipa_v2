cfd4f1ad0e52a92cbbcaf69af6a9eef3e91ee56b
higuchip <higuchip@gmail.com>
2025-05-19 15:40:32 -0300

    clean

--- activate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Activate virtual environment for TAIPA
-
-source ./venv/bin/activate
-echo "TAIPA virtual environment activated!"
-echo "To install dependencies: pip install -r requirements.txt"
-echo "To run the app: streamlit run app.py"
